ede3cd124b203ee776536f8cba8c6d5588c75fb0
Sebastián <sebasdelavega66@gmail.com>
2026-09-06 20:13:49 -0500

    fix: PWA cachea correctamente incluyendo query strings

--- sw.js
+++ sw.js
@@ -13,6 +13,9 @@ const FILES = [
   '/css/rifa.css',
   '/css/nueva-rifa.css',
   '/css/plantillas.css',
+  '/css/estadisticas.css',
+  '/css/datepicker.css',
+  '/css/about.css',
   '/js/app.js',
   '/js/rifa.js',
   '/js/nueva-rifa.js',
@@ -20,15 +23,37 @@ const FILES = [
   '/js/finalizadas.js',
   '/js/estadisticas.js',
   '/js/datepicker.js',
-  '/css/estadisticas.css',
+  '/manifest.json',
+'/assets/icons/icon-32x32.png',
+'/assets/icons/icon-192x192.png',
+'/assets/icons/icon-512x512.png',
+  
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => 
+      Promise.allSettled(FILES.map(f => c.add(f)))
+    )
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request, { ignoreSearch: true }).then(r => {
+      if (r) return r;
+      return fetch(e.request).catch(() => caches.match('/index.html'));
+    })
   );
 });
